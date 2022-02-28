d97329fd4f81bace076b26ab9230bec4f42fa91a
jini1234 <jinishat17@gmail.com>
2022-02-28 22:58:05 +0530

    primefactor for 2

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -1,6 +1,9 @@
 public class PrimeFactor {
 
     public int ComputeFactors(int number) {
+        if(number>=2){
+            return number;
+        }
         return 0;
     }
 }
--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -7,4 +7,10 @@ public class PrimeFactorTest {
         PrimeFactor pf = new PrimeFactor();
         Assert.assertEquals(0, pf.ComputeFactors(1));
     }
+
+    @Test
+    public void PrimeFactorFor2(){
+        PrimeFactor pf = new PrimeFactor();
+        Assert.assertEquals(2, pf.ComputeFactors(2));
+    }
 }
